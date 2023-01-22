c3c4361ded5e335c8a354c5da79b14f5a7c4ff28
Piotr Paczkowski <piterspp1998@gmail.com>
2023-01-22 16:58:37 +0100

    feat: added custom listing options in admin panel

--- shop/models.py
+++ shop/models.py
@@ -30,11 +30,17 @@ class Products(models.Model):
     # time of registration of the product
     created_at = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return f"{self.name} - {self.price}. Quantity left: {self.quantity}"
+
 
 class Categories(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
 
 
 class Whishlist(models.Model):
@@ -43,17 +49,26 @@ class Whishlist(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return f"{self.user_id} - {self.name}"
+
 
 class WhishlistItems(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
     whishlist_id = models.IntegerField(unique=True)
     product_id = models.IntegerField()
 
+    def __str__(self):
+        return str(self.whishlist_id)
+
 
 class ProductCategories(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
     product_id = models.IntegerField()
     category_id = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.product_id} - {self.category_id}"
 
 
 class Shipments(models.Model):
@@ -62,12 +77,20 @@ class Shipments(models.Model):
     tracking_number = models.CharField(max_length=255)
     shipped_at = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return (
+            f"{self.order_id} - {self.tracking_number}. Shipped at: {self.shipped_at}"
+        )
+
 
 class Orders(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
     user_id = models.IntegerField()
     total_price = models.FloatField(max_length=10)
     created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user_id} - total price of the orded: {self.total_price}"
 
 
 class Revievs(models.Model):
@@ -77,6 +100,9 @@ class Revievs(models.Model):
     rating = models.IntegerField(default=1)
     review = models.TextField(blank=True)
     created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Product: {self.product_id} - user: {self.user_id} rated product on {self.rating} at {self.created_at}."
 
 
 class Adresses(models.Model):
@@ -88,6 +114,9 @@ class Adresses(models.Model):
     zip_code = models.CharField(blank=False, max_length=255)
     created_at = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return f"{self.user_id} - Adress: {self.state}, {self.city}, {self.street_adress}, zip code: {self.zip_code}"
+
 
 class OrderedItems(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
@@ -95,3 +124,6 @@ class OrderedItems(models.Model):
     product_id = models.IntegerField()
     quantity = models.IntegerField()
     price = models.FloatField(max_length=10)
+
+    def __str__(self):
+        return f"{self.order_id}: containing products - {self.product_id} in quantity of {self.quantity} and total price of {self.price}"
