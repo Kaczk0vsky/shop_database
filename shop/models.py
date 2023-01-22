645491436234ce1c7a858345359616fe3e67ef66
Piotr Paczkowski <piterspp1998@gmail.com>
2023-01-22 19:10:03 +0100

    feat: added relations between objects

--- shop/models.py
+++ shop/models.py
@@ -10,7 +10,7 @@ class Users(models.Model):
     # field for entering username password string
     password = models.CharField(max_length=255)
     # field containing username email
-    e_mail = models.CharField(max_length=255)
+    e_mail = models.EmailField(max_length=255)
     # field that gets the time of registration
     created_at = models.DateTimeField(default=timezone.now)
 
@@ -31,7 +31,19 @@ class Products(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"{self.name} - {self.price}. Quantity left: {self.quantity}"
+        return f"{self.name} - {self.price}"
+
+
+class Revievs(models.Model):
+    id = models.IntegerField(unique=True, primary_key=True)
+    product_id = models.ManyToManyField(Products)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=1)
+    review = models.TextField(blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user_id} rated products on {self.rating} at {self.created_at}."
 
 
 class Categories(models.Model):
@@ -43,9 +55,18 @@ class Categories(models.Model):
         return self.name
 
 
+class ProductCategories(models.Model):
+    id = models.IntegerField(unique=True, primary_key=True)
+    product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.product_id} - {self.category_id}"
+
+
 class Whishlist(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
-    user_id = models.IntegerField()
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(default=timezone.now)
 
@@ -55,59 +76,39 @@ class Whishlist(models.Model):
 
 class WhishlistItems(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
-    whishlist_id = models.IntegerField(unique=True)
-    product_id = models.IntegerField()
+    whishlist_id = models.ForeignKey(Whishlist, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.whishlist_id)
-
-
-class ProductCategories(models.Model):
-    id = models.IntegerField(unique=True, primary_key=True)
-    product_id = models.IntegerField()
-    category_id = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.product_id} - {self.category_id}"
-
-
-class Shipments(models.Model):
-    id = models.IntegerField(unique=True, primary_key=True)
-    order_id = models.IntegerField()
-    tracking_number = models.CharField(max_length=255)
-    shipped_at = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return (
-            f"{self.order_id} - {self.tracking_number}. Shipped at: {self.shipped_at}"
-        )
+        return f"{self.id} - {self.whishlist_id}"
 
 
 class Orders(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
-    user_id = models.IntegerField()
-    total_price = models.FloatField(max_length=10)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    total_price = models.FloatField(
+        max_length=10, editable=False, blank=True, null=True
+    )
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"{self.user_id} - total price of the orded: {self.total_price}"
 
 
-class Revievs(models.Model):
+class OrderedItems(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
-    product_id = models.IntegerField()
-    user_id = models.IntegerField()
-    rating = models.IntegerField(default=1)
-    review = models.TextField(blank=True)
-    created_at = models.DateTimeField(default=timezone.now)
+    order_id = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.FloatField(max_length=10)
 
     def __str__(self):
-        return f"Product: {self.product_id} - user: {self.user_id} rated product on {self.rating} at {self.created_at}."
+        return f"{self.order_id}: containing products - {self.product_id} in quantity of {self.quantity} and total price of {self.price}"
 
 
 class Adresses(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
-    user_id = models.IntegerField()
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
     street_adress = models.CharField(blank=False, max_length=255)
     city = models.CharField(blank=False, max_length=255)
     state = models.CharField(blank=False, max_length=255)
@@ -118,12 +119,14 @@ class Adresses(models.Model):
         return f"{self.user_id} - Adress: {self.state}, {self.city}, {self.street_adress}, zip code: {self.zip_code}"
 
 
-class OrderedItems(models.Model):
+class Shipments(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
-    order_id = models.IntegerField()
-    product_id = models.IntegerField()
-    quantity = models.IntegerField()
-    price = models.FloatField(max_length=10)
+    order_id = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    user_adress = models.ForeignKey(Adresses, on_delete=models.CASCADE)
+    tracking_number = models.CharField(max_length=255)
+    shipped_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"{self.order_id}: containing products - {self.product_id} in quantity of {self.quantity} and total price of {self.price}"
+        return (
+            f"{self.order_id} - {self.tracking_number}. Shipped at: {self.shipped_at}"
+        )
