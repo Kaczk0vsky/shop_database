b4d2fac63395292144569679e4b550f7c4b19a7f
Piotr Paczkowski <piterspp1998@gmail.com>
2023-01-22 20:07:23 +0100

    feat: added login feature to shop

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("user_login/", views.user_login, name="UserLogin"),
+    path("shop/", views.shop, name="Shop"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .models import Users
+from .forms import NewUser
 
 
-def index(request):
+def user_login(request):
+    if request.method == "POST":
+        e_mail = request.POST["e_mail"]
+        password = request.POST["password"]
+        dict = Users.objects.filter(e_mail=e_mail).values("password").get()
+        if password == dict["password"]:
+            return HttpResponseRedirect("/shop/shop/")
+        else:
+            return render(request, "user_login.html")
+
+
+def shop(request):
     return HttpResponse("Hello, world.")
